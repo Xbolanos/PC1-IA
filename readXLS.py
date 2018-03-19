d1b204cd2d9263c3b0cb67ac34f2d7abc75e7a75
Carmen <carmenmok1697@gmail.com>
2018-03-19 16:07:31 -0600

    Lee Excel y crea lista de listas

---
+++ readXLS.py
@@ -0,0 +1,24 @@
+
+
+"""
+Recibe el nombre de un archivo xls y retorna una lista de listas con los datos
+"""
+
+def read_xls (file):
+    import xlrd
+    
+    workbook = xlrd.open_workbook(str(file))
+
+    worksheet = workbook.sheet_by_index(0)
+
+    table = []
+    templist = []
+    for i in range(worksheet.nrows):
+        for j in range(worksheet.ncols):
+            templist.append(worksheet.cell(i,j).value)
+        table.append(templist)
+        templist = []
+        
+    print(table)
+
+
